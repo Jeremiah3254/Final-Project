c26d58765d564c55981042981a1daaf357be9e49
Jeremiah3254 <44614546+Jeremiah3254@users.noreply.github.com>
2020-12-15 08:09:17 -0500

    finalizing

--- FinalProject.java
+++ FinalProject.java
@@ -19,31 +19,21 @@ public class FinalProject {
         
         String spacers = "  ";
         
-        //current level
         int level = 0;
-        //current level
         
-        //
         int CurrentLives = 1;
         int livesLostOnLevel = 0;
         int livesLostOnRound = 0;
-        //
         
-        // Player's Input's
         int x = 1;
         int y = 2; 
         boolean answer = false;
-        // Player's Input's
         
-        // current box size
         int rowWidth = 2;
         int rowAmountNumber = 2;
-        // current box size
         
-        // other Variables
         int currentRowAmount = 0;
         int currentRowAmountNumber = 0;
-        // other Variables
     
         int[] boxSize = new int[250000];
         int[] answeredRows = new int[250000];
@@ -57,9 +47,6 @@ public class FinalProject {
             answeredRows[q] = 0;
         }
         
-        /*for (int i = 90; i < 100; i++) {
-            answeredRows[i] = 1;
-        }*/
     System.out.println("Current Lives: "+CurrentLives);
         for (int i = 0; i < (rowWidth*rowAmountNumber); i++) {
             
@@ -88,7 +75,6 @@ public class FinalProject {
         System.out.println("Grid Scale: (1-"+rowWidth+"),(1-"+rowAmountNumber+")");
         
         while (answer == false) {
-            //check for level
             int maxNeeded = 0;
             int currentAnswered = 0;
             for (int q = 0; q<(rowWidth*rowAmountNumber); q++) {
@@ -141,8 +127,7 @@ public class FinalProject {
                 }
                 questionAnswered(boxSize,answeredRows,rowWidth,rowAmountNumber,CurrentLives);
             }
-            //check for level
-            //changeLevel(rowWidth,rowAmountNumber,boxSize,answeredRows,livesLostOnPoints,CurrentLives,livesLostOnLevel);
+
             answer = true;
             for (int e = 0; e < (rowWidth*rowAmountNumber); e++) {
                 if (answeredRows[e] == 1 && boxSize[e] > 8 && livesLostOnPoints[e] == 0) {
@@ -152,7 +137,7 @@ public class FinalProject {
             }
             CurrentLives = CurrentLives - (livesLostOnLevel - livesLostOnRound);
             livesLostOnRound = livesLostOnLevel;
-            //check to see if any of the picked points are bombs
+
             if (CurrentLives <= 0) {
                for (int i = 0; i < (rowWidth*rowAmountNumber); i++) {
                    answeredRows[i] = 1;
@@ -160,7 +145,6 @@ public class FinalProject {
                questionAnswered(boxSize,answeredRows,rowWidth,rowAmountNumber,CurrentLives);
                System.out.println("G a m e  O v e r");
                 }else if (CurrentLives >= 1) {
-                    // stuff
                     System.out.println("Enter the cordinates of where you dont think the bombs are");
                     System.out.print("X cordinate:");
                     x = kboard.nextInt();
@@ -178,48 +162,8 @@ public class FinalProject {
             }
         }
         
-        
-       // check to see if answer is greater than 0 then repeat it all and show results
-        
-        
     }
     
-    /*public static void changeLevel(int rowWidth,int rowAmountNumber,int[] startRows,int[] answeredRows,int[] livesLostOnPoints,int CurrentLives,int livesLostOnLevel){
-        int maxNeeded = 0;
-        int currentAnswered = 0;
-        for (int q = 0; q<(rowWidth*rowAmountNumber); q++) {
-            if (startRows[q] <= 8) {
-                maxNeeded += 1;
-            }
-        }
-        for (int i = 0; i<(rowWidth*rowAmountNumber); i++) {
-            if (startRows[i] <= 8 && answeredRows[i] == 1) {
-                currentAnswered += 1;
-            }
-        }
-        
-        if (maxNeeded == currentAnswered) {
-            int calculateIncrease = (int) (rowWidth/2)+1;
-            
-            if (CurrentLives > 3) {
-                CurrentLives = CurrentLives+0;
-            } else {
-                CurrentLives = CurrentLives+(3-livesLostOnLevel);
-            }
-            for (int v = 0; v < livesLostOnPoints.length; v++) {
-                livesLostOnPoints[v] = 0;
-            }
-            if (calculateIncrease > 3) {
-                rowWidth = rowWidth + 3;
-                rowAmountNumber = rowAmountNumber + 3;
-            } else {
-                
-                rowWidth = rowWidth + calculateIncrease;
-                rowAmountNumber = rowAmountNumber + calculateIncrease;
-                System.out.println("worked kinda"+calculateIncrease+" "+rowWidth+" "+rowAmountNumber);
-            }
-        }
-    }*/
     
     public static void findCordinates(int cordinateOne, int cordinateTwo,int rowLength,int rowHeight,int[] otherStuff) {
         int calculateCordinates = (rowHeight*rowLength) - (cordinateTwo*rowLength);
@@ -227,14 +171,6 @@ public class FinalProject {
         otherStuff[finalCalculate] = 1;
     }
     
-    /* public static void prompt(int cordinateOne,int cordinateTwo,int rowWidth,int rowAmountNumber,Scanner kboard) {
-        System.out.println("Grid Scale: (1-"+rowWidth+"),(1-"+rowAmountNumber+")");
-        System.out.println("Enter the cordinates of where you dont think the bombs are");
-       System.out.print("X cordinate:");
-        cordinateOne = kboard.nextInt();
-        System.out.print("Y cordinate:");
-        cordinateTwo = kboard.nextInt();
-    }*/
     
      public static void questionAnswered(int[] boxSize,int[] answeredRows,int rowWidth,int rowAmountNumber,int CurrentLives) {
         
@@ -268,15 +204,4 @@ public class FinalProject {
         System.out.println("Grid Scale: (1-"+rowWidth+"),(1-"+rowAmountNumber+")");
         
     }
-    /*
-    public static void checkAmountOfLives(int currentLives, boolean lost,int[] boxSize,int[] boxAnswers,int rowWidth,int rowAmountNumber) {
-        if (currentLives <= 0) {
-           lost = true; 
-           for (int i = 0; i < (rowWidth*rowAmountNumber); i++) {
-               boxAnswers[i] = 1;
-           }
-           questionAnswered(boxSize,boxAnswers,rowWidth,rowAmountNumber);
-           System.out.println("G a m e  O v e r");
-        } 
-    }*/
 }
